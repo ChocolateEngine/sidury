da6245caa84ad5b197cf5d1ca7c7ca9704de0696
demez <demizexp@gmail.com>
2024-03-23 15:49:49 -0400

    graphics: add LineWidth dynamic state to debug shaders

--- src/graphics/shaders/shader_debug.cpp
+++ src/graphics/shaders/shader_debug.cpp
@@ -41,7 +41,7 @@ static void Shader_Debug_GetGraphicsPipelineCreate( GraphicsPipelineCreate_t& sr
 
 	srGraphics.aPrimTopology = EPrimTopology_Tri;
 	srGraphics.aLineMode     = true;
-	srGraphics.aDynamicState = EDynamicState_Viewport | EDynamicState_Scissor;
+	srGraphics.aDynamicState = EDynamicState_Viewport | EDynamicState_Scissor | EDynamicState_LineWidth;
 	srGraphics.aCullMode     = ECullMode_None;
 }
 
@@ -82,7 +82,7 @@ ShaderCreate_t gShaderCreate_Debug = {
 	.aStages          = ShaderStage_Vertex | ShaderStage_Fragment,
 	.aBindPoint       = EPipelineBindPoint_Graphics,
 	.aFlags           = EShaderFlags_PushConstant,
-	.aDynamicState    = EDynamicState_Viewport | EDynamicState_Scissor,
+	.aDynamicState    = EDynamicState_Viewport | EDynamicState_Scissor | EDynamicState_LineWidth,
 	.aVertexFormat    = VertexFormat_Position,
 	.apInit           = nullptr,
 	.apDestroy        = nullptr,
@@ -204,7 +204,7 @@ ShaderCreate_t gShaderCreate_Wireframe = {
 	.aStages          = ShaderStage_Vertex | ShaderStage_Fragment,
 	.aBindPoint       = EPipelineBindPoint_Graphics,
 	.aFlags           = EShaderFlags_PushConstant,
-	.aDynamicState    = EDynamicState_Viewport | EDynamicState_Scissor,
+	.aDynamicState    = EDynamicState_Viewport | EDynamicState_Scissor | EDynamicState_LineWidth,
 	.aVertexFormat    = VertexFormat_Position,
 	.apInit           = nullptr,
 	.apDestroy        = nullptr,
